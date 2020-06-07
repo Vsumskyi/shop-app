8973b111f9597716f4313dd6a5c9646dc8167f2b
Vsumskiy <im.vasia13@gmail.com>
2020-06-07 12:55:09 +0300

    formating

--- src/Components/Pages/Cart/Cart.js
+++ src/Components/Pages/Cart/Cart.js
@@ -5,34 +5,33 @@ import classes from './Cart.module.scss'
 import { CartItem } from './CartItem/CartItem'
 
 export const CartComponent = ({ cartItems, countPrice, price }) => {
-	useEffect(() => {
-		countPrice()
-	})
+  useEffect(() => {
+    countPrice()
+  })
 
-	const renderProducts = () => (
-		cartItems.map(item => <CartItem key={item.id} product={item} />)
-	)
-	
-	return (
-		<div className={classes.Cart}>
-			{<div className={classes.products}>{renderProducts()}</div>}
-			{cartItems.length < 1 ? (
-				<p>
-					Корзина пуста!
-					<br />
-					Вы можете продолжить покупки в&nbsp;
-					<NavLink to={'/'}>каталоге товаров</NavLink>{' '}
-				</p>
-			) : (
-				<div className={classes.calc}>
-					<div>
-						<span>
-							Сумма: <h2>{price}</h2> грн
-						</span>
-					</div>
-					<button>Купить сейчас</button>
-				</div>
-			)}
-		</div>
-	)
+  const renderProducts = () =>
+    cartItems.map(item => <CartItem key={item.id} product={item} />)
+
+  return (
+    <div className={classes.Cart}>
+      {<div className={classes.products}>{renderProducts()}</div>}
+      {cartItems.length < 1 ? (
+        <p>
+          Корзина пуста!
+          <br />
+          Вы можете продолжить покупки в&nbsp;
+          <NavLink to={'/'}>каталоге товаров</NavLink>{' '}
+        </p>
+      ) : (
+        <div className={classes.calc}>
+          <div>
+            <span>
+              Сумма: <h2>{price}</h2> грн
+            </span>
+          </div>
+          <button>Купить сейчас</button>
+        </div>
+      )}
+    </div>
+  )
 }
